7256c37de467f4c8219c3d67054ec9dacbbcf403
Th0-Th0 <87905419+Th0-Th0@users.noreply.github.com>
2024-03-10 08:54:15 +0000

    Unit test

---
+++ src/test/java/com/tho/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.tho;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testHelloWorld() {
+        Assertions.assertTrue(true);
+    }
+}
